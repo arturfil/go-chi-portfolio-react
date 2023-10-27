3ca4b73ae57436dd0a78346d996a72d1c41acfec
arturfil <arturfil@hotmail.com>
2023-10-27 17:35:51 -0400

    added refactor for static image data

--- interfaces/Project.ts
+++ interfaces/Project.ts
@@ -1,9 +1,11 @@
+import { StaticImageData } from "next/image";
+
 export interface Project {
     id?: string;
     name: string;
     author: string;
     url: string;
-    img?: string;
+    img?: string | StaticImageData;
     description: string;
     created_at?: string;
     updated_at?: string;
